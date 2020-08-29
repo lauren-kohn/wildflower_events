893b96900db83497b50e014e6a91b40aa3af14c1
lauren-kohn <65028543+lauren-kohn@users.noreply.github.com>
2020-08-29 15:51:18 -0500

    builds initial functionality for call method, get_user_input

--- lib/wildflower_events.rb
+++ lib/wildflower_events.rb
@@ -1,6 +1,8 @@
 require_relative "./wildflower_events/version"
 require_relative "./wildflower_events/cli"
 
+require 'pry'
+
 
 module WildflowerEvents
   class Error < StandardError; end
--- lib/wildflower_events/cli.rb
+++ lib/wildflower_events/cli.rb
@@ -1,8 +1,18 @@
 class WildflowerEvents::CLI
 
-	def greeting
-		puts "Welcome to the Wildflower Events app! What are you interested in?"
-		#gets.strip
+	def call
+		puts "Welcome to the Wildflower Events app! Please enter a search term to find events."
+		puts "Consider an upcoming month name or a type of activity, such as yoga or gardening."
+		get_user_input
+		# get_events(input)
+		# list_events 
+		# select_from_list(input)
+		# get_event_detail
+		# present_details
+	end
+	
+	def get_user_input 
+	  gets.strip
 	end
 
 end
