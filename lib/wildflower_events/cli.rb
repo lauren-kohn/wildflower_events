49889cd4ef9f96f296db74c76ce5b5c02f554e95
lauren-kohn <65028543+lauren-kohn@users.noreply.github.com>
2020-08-30 12:26:16 -0500

    builds #call, #get_user_input, #list_events, #select_from_list

--- lib/wildflower_events/cli.rb
+++ lib/wildflower_events/cli.rb
@@ -4,15 +4,35 @@ class WildflowerEvents::CLI
 		puts "Welcome to the Wildflower Events app! Please enter a search term to find events."
 		puts "Consider an upcoming month name or a type of activity, such as yoga or gardening."
 		get_user_input
-		# get_events(input)
-		# list_events 
-		# select_from_list(input)
-		# get_event_detail
+		# get_events(input) (scraper class)
+		list_events 
+		select_from_list
+		# get_event_detail (scraper class)
 		# present_details
 	end
 	
 	def get_user_input 
-	  gets.strip
+	  @user_search = gets.strip
+		# this info will be used for the scraper
 	end
+	
+	def list_events
+		# info from scraper
+		@events = ["yoga", "gardening", "walk with naturalist"]
+		puts "Please select a number from the list."
+		@events.each_with_index do |event, index|
+			puts "#{index + 1}. #{event}"
+		end
+	end 
+	
+	def select_from_list
+		@selected_event = gets.strip
+	end
+	
+	def present_details 
+		# puts event_name
+		# puts event_details
+	end
+		# add options to repeat or exit
 
 end
